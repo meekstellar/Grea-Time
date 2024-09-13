e5bb7d7322cb174eba698f408b8526211061d47e
petro-skotar <162284308+petro-skotar@users.noreply.github.com>
2024-09-13 13:12:07 -0700

    When phone is empty

--- app/Http/Controllers/CodeController.php
+++ app/Http/Controllers/CodeController.php
@@ -16,37 +16,43 @@ class CodeController extends Controller
 
         // Відображуємо телефон користувача
         $userPhone = auth()->user()->phone;
-        $userPhone = '+79067916085';
-        $userPhone_hide = '';
-        for($i=0;$i<strlen($userPhone)-3;$i++){
-            $userPhone_hide .= '*';
-        }
-        $userPhone_hide .= substr($userPhone, -3);
-        if(!session('sms_code')){
-            // тут відправляємо смс через сервіс sms.ru
-            // і очікуємо відповіді
-            # Раскоменить, если будет подключаться проверка по смс
 
-            // хай буде такий:
-            $array = [1,2,3,4,5,6,7,8,9,'R','D','F','W','G','S'];
-            $sms_code = $array[rand(0,count($array)-1)]
-                        .$array[rand(0,count($array)-1)]
-                        .$array[rand(0,count($array)-1)]
-                        .$array[rand(0,count($array)-1)];
-            //$sms_code = '12345';
+        if(!empty($userPhone)){
+            $userPhone_hide = '';
+            for($i=0;$i<strlen($userPhone)-3;$i++){
+                $userPhone_hide .= '*';
+            }
+            $userPhone_hide .= substr($userPhone, -3);
+            if(!session('sms_code')){
+                // тут відправляємо смс через сервіс sms.ru
+                // і очікуємо відповіді
+                # Раскоменить, если будет подключаться проверка по смс
 
-            $body = file_get_contents("https://sms.ru/sms/send?api_id=3505F8C6-5202-07F1-18D4-E19EF58B41A8&to=".$userPhone."&ip=".$_SERVER['REMOTE_ADDR']."&msg=".urlencode("Ваш проверочный код: ".$sms_code)."&json=1"); # Если приходят крякозябры, то уберите iconv и оставьте только urlencode("Привет!")
-            $json = json_decode($body);
-            //dd($sms_code, $json);
-            # dd($json);
-            // В сеісію записуємо код з смс
-            session(['sms_code' => $sms_code]);
-            $sended = 1;
+                // хай буде такий:
+                $array = [1,2,3,4,5,6,7,8,9,'R','D','F','W','G','S'];
+                $sms_code = $array[rand(0,count($array)-1)]
+                            .$array[rand(0,count($array)-1)]
+                            .$array[rand(0,count($array)-1)]
+                            .$array[rand(0,count($array)-1)];
+                //$sms_code = '12345';
+
+                $body = file_get_contents("https://sms.ru/sms/send?api_id=3505F8C6-5202-07F1-18D4-E19EF58B41A8&to=".$userPhone."&ip=".$_SERVER['REMOTE_ADDR']."&msg=".urlencode("Your code: ".$sms_code)); # Если приходят крякозябры, то уберите iconv и оставьте только urlencode("Привет!")
+                $json = json_decode($body);
+                //dd($sms_code, $json);
+                # dd($json);
+                // В сеісію записуємо код з смс
+                session(['sms_code' => $sms_code]);
+                $sended = 1;
+            } else {
+                $sended = 0;
+            }
+
+            return view('auth.enter_code', compact(['userPhone_hide','sended']));
         } else {
-            $sended = 0;
-        }
+            session(['special_code' => 'CODE']); // Якщо не вказаний номер телефону, то пропускаєжмо без смс-коду
 
-        return view('auth.enter_code', compact(['userPhone_hide','sended']));
+            return redirect('workers');
+        }
     }
 
     // Обробляємо введений код
